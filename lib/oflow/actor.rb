81f1ba17093a40e9a0882f56be49501b3107c992
Peter Ohler <peter@ohler.com>
2014-01-02 20:57:12 -0800

    started on actor and tesk

--- lib/oflow/actor.rb
+++ lib/oflow/actor.rb
@@ -2,6 +2,13 @@
 module OFlow
 
   class Actor
+
+    def initialize()
+    end
+
+    def perform(task, op, box)
+    end
+
   end
 
 end # OFlow
--- lib/oflow/task.rb
+++ lib/oflow/task.rb
@@ -2,7 +2,23 @@
 module OFlow
 
   class Task
-  end
+
+    def initialize(actor_class)
+      # TBD ask class for
+      # inputs (op, description, box type)
+      # description
+      # out (name, box type, description)
+      # verify instances support the perform method
+    end
+
+    def receive(op, box)
+
+    end
+
+    def ship(dest, box)
+      # TBD lookup dest to get a task and op, then call receive on that task
+    end
+
+  end # Task
 
 end # OFlow
-
