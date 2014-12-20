f61dfa80f50ded59aaab28e7285831775236efd6
Peter Ohler <peter@ohler.com>
2014-12-20 10:37:44 -0800

    added sorting

--- examples/gemchart/pagemaker.rb
+++ examples/gemchart/pagemaker.rb
@@ -29,6 +29,9 @@ class PageMaker < ::OFlow::Actor
         year,mon,day = rec[:date].split('.')
         set << [Time.new(year.to_i, mon.to_i, day.to_i).to_i()/86400, rec[:downloads], rec[:date], rec[:version]]
       }
+      sets.each do |g,s|
+        s.sort! { |a,b| a[0] <=> b[0] }
+      end
       json = ::Oj.dump(sets, mode: :compat, indent: 2)
     end
     out_box = ::OFlow::Box.new(out)
